7f955508eb5ae5d5d5f2160fd614e73bb3de6fc0
hugosenari <hugosenari@gmail.com>
2022-03-12 02:15:04 -0300

    chore: re index ogham

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Convert Ogham inscriptions to latin text & vice versa.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."ogham-main".url = "path:./main";
+  inputs."ogham-main".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."ogham-main".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
